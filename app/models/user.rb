b10283014193c0b4e80c4471c70d91f8d79a73bc
lucas <lucas.tx.reis@gmail.com>
2023-10-10 00:20:36 -0300

    feat: user w/ email, cpf & phone validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    validates :name, :email, :cpf, presence: { message: MessagesHelper.blank }
+
+    validates :email, email: { message: MessagesHelper.invalid }, uniqueness: { message: MessagesHelper.already_registered }
+
+    validates :cpf, cpf: { message: MessagesHelper.invalid }
+
+    validates :phone, phone: { message: MessagesHelper.invalid }
 end
