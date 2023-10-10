0e479ffdea3d1d673a6f231d6201a0382ef1c25f
lucas <lucas.tx.reis@gmail.com>
2023-10-10 00:19:38 -0300

    feat: messages_helper to aggregate display texts

---
+++ app/helpers/messages_helper.rb
@@ -0,0 +1,13 @@
+module MessagesHelper
+    def self.invalid
+        "inválido!"
+    end
+
+    def self.already_registered
+        "já cadastrado."
+    end
+
+    def self.blank
+        "não pode ser vazio."
+    end
+end
